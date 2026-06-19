e5d76a99f1c3c558641cd7c9feae884561de8927
Lakshmi Chaturya <slchaturya@gmail.com>
2026-06-19 14:27:42 +0530

    added javascript file

--- Frontend/javascript-practice.js
+++ Frontend/javascript-practice.js
@@ -43,15 +43,27 @@
 //     console.log("")
 // }
 
-const a =10
-const b=20
-const c =15
-if((a>b)&&(a>c)){
-    console.log(a)
-}
-else if((b>a)&&(b>c)){
-    console.log(b)
+// const a =10
+// const b=20
+// const c =15
+// if((a>b)&&(a>c)){
+//     console.log(a)
+// }
+// else if((b>a)&&(b>c)){
+//     console.log(b)
+// }
+// else{
+//     console.log(c)
+// }
+
+
+const age = 19
+const license = true
+if(age>=18){
+    if(license){
+        console.log("can drive")
+    }
 }
 else{
-    console.log(c)
+    console.log("cannot drive")
 }
