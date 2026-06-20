310d1a0325c10a4176299d7d8cf44303f29030b0
Lakshmi Chaturya <slchaturya@gmail.com>
2026-06-20 12:48:47 +0530

    new js file

--- Frontend/javascript-practice.js
+++ Frontend/javascript-practice.js
@@ -268,6 +268,124 @@
 // console.log(filter_arr)
 // console.log(filter_arr.length)
 
-//reduce and break
+//reduce it uses two paramters function and value
+// 
 
 
+
+
+
+// const students = [
+//   { rollNo: 101, name: "Arun", department: "CSE", marks: 85 },
+//   { rollNo: 102, name: "Ravi", department: "CSD", marks: 22 },
+//   { rollNo: 103, name: "Sita", department: "CSM", marks: 78 },
+//   { rollNo: 104, name: "Kiran", department: "IT", marks: 45 },
+//   { rollNo: 105, name: "Priya", department: "CSO", marks: 91 },
+
+//   { rollNo: 106, name: "Rahul", department: "CSE", marks: 30 },
+//   { rollNo: 107, name: "Sneha", department: "CSD", marks: 67 },
+//   { rollNo: 108, name: "Teja", department: "CSM", marks: 55 },
+//   { rollNo: 109, name: "Divya", department: "IT", marks: 29 },
+//   { rollNo: 110, name: "Ajay", department: "CSO", marks: 72 },
+
+//   { rollNo: 111, name: "Anil", department: "CSE", marks: 95 },
+//   { rollNo: 112, name: "Meena", department: "CSD", marks: 41 },
+//   { rollNo: 113, name: "Vamsi", department: "CSM", marks: 18 },
+//   { rollNo: 114, name: "Nisha", department: "IT", marks: 88 },
+//   { rollNo: 115, name: "Kavya", department: "CSO", marks: 36 },
+
+//   { rollNo: 116, name: "Ramesh", department: "CSE", marks: 50 },
+//   { rollNo: 117, name: "Pooja", department: "CSD", marks: 93 },
+//   { rollNo: 118, name: "Tarun", department: "CSM", marks: 34 },
+//   { rollNo: 119, name: "Deepika", department: "IT", marks: 62 },
+//   { rollNo: 120, name: "Manoj", department: "CSO", marks: 28 },
+
+//   { rollNo: 121, name: "Lokesh", department: "CSE", marks: 74 },
+//   { rollNo: 122, name: "Anusha", department: "CSD", marks: 81 },
+//   { rollNo: 123, name: "Sai", department: "CSM", marks: 39 },
+//   { rollNo: 124, name: "Harika", department: "IT", marks: 27 },
+//   { rollNo: 125, name: "Keerthi", department: "CSO", marks: 58 },
+
+//   { rollNo: 126, name: "Bhanu", department: "CSE", marks: 33 },
+//   { rollNo: 127, name: "Mahesh", department: "CSD", marks: 46 },
+//   { rollNo: 128, name: "Jyothi", department: "CSM", marks: 87 },
+//   { rollNo: 129, name: "Chandu", department: "IT", marks: 53 },
+//   { rollNo: 130, name: "Suresh", department: "CSO", marks: 20 }
+// ];
+
+// // display all students
+// console.log(students) 
+// // passed students 
+// const passedStudents = students.filter(student=>student.marks>35)
+// console.log(passedStudents)
+
+// const passedStudentsCSM = students.filter(student=>student.marks>35 && student.department ==="CSM")
+// console.log(passedStudentsCSM)
+// const passedStudentsCSE = students.filter(student=>student.marks>35 && student.department ==="CSE")
+// console.log(passedStudentsCSE)
+// const passedStudentsCSD = students.filter(student=>student.marks>35 && student.department ==="CSD")
+// console.log(passedStudentsCSD)
+
+
+const students = [
+  { rollNo: 101, name: "Arun", department: "CSE", marks: 85, placed: true, package: 12 },
+  { rollNo: 102, name: "Ravi", department: "IT", marks: 65, placed: false, package: 0 },
+  { rollNo: 103, name: "Sita", department: "CSD", marks: 92, placed: true, package: 15 },
+  { rollNo: 104, name: "Rahul", department: "CSM", marks: 55, placed: true, package: 6 },
+  { rollNo: 105, name: "Priya", department: "CSO", marks: 88, placed: true, package: 18 },
+  { rollNo: 106, name: "Ajay", department: "CSE", marks: 72, placed: false, package: 0 },
+  { rollNo: 107, name: "Sneha", department: "IT", marks: 81, placed: true, package: 10 },
+  { rollNo: 108, name: "Kiran", department: "CSD", marks: 49, placed: false, package: 0 },
+  { rollNo: 109, name: "Deepika", department: "CSM", marks: 90, placed: true, package: 14 },
+  { rollNo: 110, name: "Manoj", department: "CSO", marks: 76, placed: true, package: 8 }
+];
+//placed students
+const placedd=students.filter(students=>students.placedd)
+console.log(placedd)
+
+// from placed name and package
+const placedStudents = students
+    .filter(student => student.placed)
+    .map(student => ({
+        name: student.name,
+        package: student.package
+    }));
+
+console.log(placedStudents);
+// const placedStudents = placedStudents.map(student=>({
+//     name:student.name,
+//     package:student.package
+// }))
+// console.log(placedStudents)
+
+
+// const highest = highest.sort
+
+// avg package
+const placed = students.filter(student => student.placed);
+
+const avgPackage =
+    placed.reduce((sum, student) => sum + student.package, 0) /
+    placed.length;
+
+console.log(avgPackage);
+//highest package
+
+//more than 10 lpa
+
+const lpa = students.filter(students=>students.placed>10)
+console.log(lpa)
+
+//  const highest=placedStudents.reduce((max,student)=> max > student.package? max:student.package,0);
+//  console.log(highest)
+
+const highest = placedStudents.reduce(
+    (max, student) => max > student.package ? max : student.package,
+    0
+);
+
+console.log(highest);
+
+
+const morethan10lpa=placedStudents.filter(student=>student.package>10).map(student=>student.name)
+console.log(morethan10lpa)
